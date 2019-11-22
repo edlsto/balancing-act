7648b71ba3f15a9c14def39c777bfcb468769959
Edward Stoner <edwardlstoner@gmail.com>
2019-11-21 21:53:37 -0700

    Add transition

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
 const btn = document.getElementById('closebtn');
 btn.addEventListener('click', event => {
-  btn.parentElement.style.display = 'none'
+  btn.parentElement.classList.add('transition');
 });
